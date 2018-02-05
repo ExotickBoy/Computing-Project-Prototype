7abd19e0e8416375c5eb4573ef990fd7c2df74d2
Kacper Lubisz <kacperlubisz@gmail.com>
2018-02-05 18:58:53 +0000

    BugFix

--- src/dialogs/NewRecordingDialog.kt
+++ src/dialogs/NewRecordingDialog.kt
@@ -243,31 +243,18 @@ class NewRecordingDialog(private val application: MainApplication, private val r
      * It allows for handling the creation of a new tuning
      */
     fun refresh(newTuning: Tuning?) {
+        if (newTuning != null) {
 
-        if (customTuning == null) { // there isn't already a custom tuning
+            if (customTuning == null) { // there isn't already a custom tuning
 
-            if (newTuning == null) { // if a new tuning was created
-
-                tuningComboBox.selectionModel.select(0)
-
-            } else { // added
 
                 tuningComboBox.items[tuningComboBox.items.lastIndex] = newTuning.name
                 tuningComboBox.items.add(EDIT_CUSTOM_TUNING_PREFIX + newTuning.name)
 
                 // the new one will already be selected
-            }
 
-        } else {
-
-            if (newTuning == null) {
-
-                tuningComboBox.selectionModel.select(0)
-                tuningComboBox.items.removeAt(tuningComboBox.items.lastIndex)
-                tuningComboBox.items[tuningComboBox.items.lastIndex] = MAKE_TUNING_TEXT
-
-
-            } else { // changed
+            } else {
+                // changed
 
                 tuningComboBox.items[tuningComboBox.items.lastIndex - 1] = newTuning.name
                 tuningComboBox.items[tuningComboBox.items.lastIndex] = EDIT_CUSTOM_TUNING_PREFIX + newTuning.name
